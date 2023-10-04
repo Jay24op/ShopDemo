7c3a5dc2c4ba8bb12fb8817491dc5b7d0247ef7a
Jay24op <Jaymanmw2442@gmail.com>
2023-10-03 18:09:41 -0600

    swiper navbtn defined

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ document.querySelector('#menu-bar').onclick=() =>{
 
 var swiper = new Swiper(".product-row", {
     spaceBetween: 30,
+    loop: true,
     centeredSlides:true, //centering slide to screen
     autoplay: {
         delay:9500,
@@ -31,16 +32,22 @@ var swiper = new Swiper(".product-row", {
       },
     },
   });
+
 var swiper = new Swiper(".blogs-row", {
-    spaceBetween: 30,
+    loop: true,
     centeredSlides:true, //centering slide to screen
     autoplay: {
         delay:9500,
         disableOnInteraction:false,
     },
+    observer: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     breakpoints: {
       600: {
